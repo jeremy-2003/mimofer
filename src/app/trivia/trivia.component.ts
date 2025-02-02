a50631c9386ecb383f306bd12beff3e69d7939ec
Jeremy Alcala <jeremyalcalaxcz@hotmail.com>
2025-02-02 03:55:53 -0500

    finalizado preguntas

--- src/app/trivia/trivia.component.ts
+++ src/app/trivia/trivia.component.ts
@@ -15,6 +15,12 @@ export class TriviaComponent {
     { pregunta: '¿Como se llama el primer restaurante que fuimos?', opciones: ['Mi Sociedad Privada', 'Mi Propiedad Privada', 'Mercado Aviacion', 'Museo Laco'], respuesta: 'Mercado Aviacion' },
     { pregunta: '¿Cual es nuestro lugar?', opciones: ['Al lado del teatro nacional', 'Parque frente plaza SM', 'La Rambla', 'Plaza San Miguel'], respuesta: 'Al lado del teatro nacional' },
     { pregunta: '¿Quien dio el primero beso/pico a quien?', opciones: ['Tu a mi', 'Yo a ti', 'Mutuo acuerdo', 'No paso hasta ser enamorados'], respuesta: 'Yo a ti' },
+    { pregunta: '¿Cual fue el primer objeto compartido que tuvimos?', opciones: ['Un collar de corazon', 'Un collar de mineral', 'Una pulsera negra', 'Unos anillos'], respuesta: 'Un collar de mineral' },
+    { pregunta: '¿Que cosa fue lo que mas me gusto(Jeremy) de nuestra primera salida?', opciones: ['Los bolos', 'Bailar', 'El almuerzo', 'El abrazo'], respuesta: 'El abrazo' },
+    { pregunta: '¿En qué fecha nos convertimos en enamorados oficialmente?', opciones: ['03/12/2024', '13/12/2024', '03/01/2025', '13/10/2024'], respuesta: '03/12/2024' },
+    { pregunta: '¿Qué color de ropa usaba(Jeremy) en nuestra primera cita?', opciones: ['Negra-Beich', 'Negra-Jean', 'Negro', 'Beich'], respuesta: 'Negra-Beich' },
+    { pregunta: '¿Cuál fue la primera película/serie que vimos juntos?', opciones: ['Hotel Hazbin', 'Black Clover', 'Suits', 'Arcane'], respuesta: 'Hotel Hazbin' },
+    { pregunta: '¿Que es lo que yo(Jeremy) te digo mas?', opciones: ['Te quiero', 'Que guapa', 'Eres hermosa', 'Te amo', 'Todas son Correctas'], respuesta: 'Todas son Correctas' },
   ];
 
   preguntaActual = 0;
